b1d5b1ffaf74131e8b01f5bcebef43dfe9901a82
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-05-07 15:11:41 -0400

    Update server.py :3

--- server.py
+++ server.py
@@ -26,4 +26,4 @@ def handle_client_datach3(data):
     socketio.emit('response_chschoolstuff', {'message': f"{data['content']}"})
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app, allow_unsafe_werkzeug=True, host='0.0.0.0')
